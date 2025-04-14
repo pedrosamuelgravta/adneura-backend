265dee4177f18a8770db73d074063ed266ac03b3
PsSave <pedrosviki1265@gmail.com>
2025-04-13 23:48:10 -0300

    Add new allowed origin for CORS configuration in server.js

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Configure CORS - adjust origin to your frontend domain
-const allowedOrigins = ["http://localhost:5173", "https://gravta.com"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gravta.com",
+  "https://adneura-backend-uchgev-f09b61-194-195-86-246.traefik.me",
+];
 
 app.use(
   cors({
